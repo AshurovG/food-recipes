3cd6d73e050cb1550aad51d263a18420d8158d3d
Ashurov <ashurov1308@mail.ru>
2023-09-06 22:05:14 +0300

    add input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './Input.css'
+
+export type InputProps = Omit<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    'onChange' | 'value'
+> & {
+    /** Значение поля */
+    value?: string;
+    /** Callback, вызываемый при вводе данных в поле */
+    onChange: (value: string) => void;
+    /** Слот для иконки справа */
+    afterSlot?: React.ReactNode;
+    disabled?: Boolean;
+    className?: string;
+    onclick?: () => void;
+};
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ value, onChange, onClick, afterSlot, placeholder, disabled, className, ...rest }, ref) => {
+    let newValue = value
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newValue = e.target.value
+        onChange(newValue)
+
+    };
+
+    let classes: string = 'input_container '
+    if (className) {
+        classes += className
+    }
+
+    return (
+        <div onClick={onClick} className={classes}>
+            <input ref={ref} data-testid="input" {...rest} disabled={disabled} className='input_area' type="text" placeholder={placeholder} onChange={handleInputChange} value={newValue} />
+            {afterSlot && afterSlot}
+        </div>
+    );
+});
+
+export default Input;
