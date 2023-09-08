a6ecc04e0191130bd923953fdf2d1f866193aeec
Ashurov <ashurov1308@mail.ru>
2023-09-08 21:07:41 +0300

    add main image component and add it in recipes page

---
+++ src/components/MainImage/index.ts
@@ -0,0 +1,2 @@
+export { default } from './MainImage';
+export * from './MainImage';
--- src/pages/RecipesPage/RecipesPage.tsx
+++ src/pages/RecipesPage/RecipesPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import cn from 'classnames';
 import Header from 'components/Header';
+import MainImage from 'components/MainImage';
 import styles from './RecipesPage.module.scss';
 
 const RecipesPage: React.FC = () => {
     return (
         <div className={styles.recipes__page}>
             <Header></Header>
+            <MainImage />
         </div>
     )
 };
