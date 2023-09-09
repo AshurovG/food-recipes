d35448f0464d7d2d943a18ea7a7453b97e764099
Ashurov <ashurov1308@mail.ru>
2023-09-09 20:36:50 +0300

    create detailed info component

---
+++ src/components/DetailedInfo/DetailedInfo.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './DetailedInfo.module.scss'
+import IngredientIcon from 'components/icons/IngredientIcon';
+import EquipmentIcon from 'components/icons/EquipmentIcon';
+
+export type DetailedInfoProps = {
+    type: 'ingredients' | 'equipment';
+    children: React.ReactNode;
+};
+
+const DetailedInfo: React.FC<DetailedInfoProps> = ({
+    type,
+    children,
+}) => {
+
+    return (
+        <div className={styles.detailed}>
+            {type === 'ingredients' ? <IngredientIcon className={styles.detailed__icon} /> : <EquipmentIcon className={styles.detailed__icon} />}
+            <div className='detailed__text'>{children}</div>
+        </div>
+    )
+};
+
+export default DetailedInfo;
