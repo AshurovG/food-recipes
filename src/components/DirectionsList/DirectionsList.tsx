9a5d1e6a333e46a2015f54b454cecbc4973d7bb5
Ashurov <ashurov1308@mail.ru>
2023-09-09 20:53:51 +0300

    create directions list component

---
+++ src/components/DirectionsList/DirectionsList.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './DirectionsList.module.scss'
+import Text from 'components/Text';
+
+type DirectionsInfo = {
+    title: string,
+    text: string
+}
+
+export type DirectionsListProps = {
+    steps: Array<DirectionsInfo>
+};
+
+const DirectionsList: React.FC<DirectionsListProps> = ({
+    steps
+}) => {
+
+    return (
+        <div className={styles.directions__list}>
+            {steps.map((step: DirectionsInfo) =>
+                <div className={styles['directions__list-item']}>
+                    <Text view='p-16' weight='bold' className={styles['directions__list-title']}>{step.title}</Text>
+                    <Text view='p-14' className={styles['directions__list-text']}>{step.text}</Text>
+                </div>
+            )}
+        </div>
+    )
+};
+
+export default DirectionsList;
