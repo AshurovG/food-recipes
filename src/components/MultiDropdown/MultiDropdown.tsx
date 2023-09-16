ce9798f09685c26983be686c6405633d5bddfe89
Ashurov <ashurov1308@mail.ru>
2023-09-16 06:10:04 +0300

    delete useless comments from dropdown component

--- src/components/MultiDropdown/MultiDropdown.tsx
+++ src/components/MultiDropdown/MultiDropdown.tsx
@@ -6,24 +6,16 @@ import Text from 'components/Text';
 import ArrowDownIcon from 'components/icons/ArrowDownIcon';
 
 export type Option = {
-    /** Ключ варианта, используется для отправки на бек/использования в коде */
     key: string;
-    /** Значение варианта, отображается пользователю */
     value: string;
 };
 
-/** Пропсы, которые принимает компонент Dropdown */
 export type MultiDropdownProps = {
     className?: string;
-    /** Массив возможных вариантов для выбора */
     options: Option[];
-    /** Текущие выбранные значения поля, может быть пустым */
     value: Option[];
-    /** Callback, вызываемый при выборе варианта */
     onChange: (value: Option[]) => void;
-    /** Заблокирован ли дропдаун */
     disabled?: boolean;
-    /** Возвращает строку которая будет выводится в инпуте. В случае если опции не выбраны, строка должна отображаться как placeholder. */
     getTitle: (value: Option[]) => string;
 };
 
@@ -56,15 +48,6 @@ const MultiDropdown: React.FC<MultiDropdownProps> = ({
         setIsOpen(true)
         setIsTyping(true)
     }, [disabled])
-
-    //     selectedSet.forEach((item) => {
-    //         if (item.key === selectedOption.key) {
-    //             console.log(11111)
-    //             onChange(value.filter((o) => o.key !== selectedOption.key))
-    //             return
-    //         }
-    //     });
-    // }
 
     const onClickOption = (selectedOption: Option) => () => {
         console.log(selectedSet)
