3201d7f50193d50e5db2bdb8cf89beb3192ea6ab
Ashurov <ashurov1308@mail.ru>
2023-09-16 06:32:30 +0300

    update useLocalStore by adding required destroy method

--- src/Utils/useLocalStore.ts
+++ src/Utils/useLocalStore.ts
@@ -1,10 +1,18 @@
 import React from "react";
 
-export const useLocalStore = <T>(creator: () => T): T => {
-    const container = React.useRef<null | T>(null)
+export interface ILocalStore {
+    destroy(): void;
+}
+
+export const useLocalStore = <T extends ILocalStore>(creator: () => T): T => {
+    const container = React.useRef<null | T>(null);
     if (container.current === null) {
         container.current = creator();
     }
+
+    React.useEffect(() => {
+        return () => container.current?.destroy();
+    }, [])
 
     return container.current
 }
