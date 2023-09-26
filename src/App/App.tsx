bd1209adbc848a2898f3cf04d6cf380d6d559b13
Ashurov <ashurov1308@mail.ru>
2023-09-26 14:38:49 +0300

    add meal plan page to app file to navigation

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,15 +3,21 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './App.scss'
 import RecipesPage from 'pages/RecipesPage'
 import RecipesDetailedPage from 'pages/RecipesDetailedPage'
+import MealPlanPage from 'pages/MealPlanPage';
+
 function App() {
     return (
         <div>
             <BrowserRouter>
                 <Routes>
                     <Route path='/' element={<RecipesPage />} />
+
                     <Route path='/recipe'>
                         <Route path=':id' element={<RecipesDetailedPage />} />
                     </Route>
+
+                    <Route path='/mealplan' element={<MealPlanPage />}/>
+
                     <Route path='*' element={<Navigate to="/" replace />} />
                 </Routes>
             </BrowserRouter>
