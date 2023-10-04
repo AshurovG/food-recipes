544a65e537357bce3f3a36822ebdb989b61bac90
Ashurov <ashurov1308@mail.ru>
2023-10-04 15:15:09 +0300

    fix problem with showing input value

--- consts.config.ts
+++ consts.config.ts
@@ -1,9 +1,9 @@
 
-export const apiKey = '0fc912ddd61f4f4c8c54be7a4e564f78';
+export const apiKey = '33b12c924c4c433aa2e4eab585cbc6e7';
 
 //5612ded2c55f4a42aafe5dd7fdec9f3f
-//0fc912ddd61f4f4c8c54be7a4e564f78 - deploy
-//33b12c924c4c433aa2e4eab585cbc6e7
+//0fc912ddd61f4f4c8c54be7a4e564f78 
+//33b12c924c4c433aa2e4eab585cbc6e7 - deploy
 //f9504a4e01dc4ded9e9ce434792cac15
 //e60b5aa70d724292a5d1c92ffe47aba9
 //df12787acb5745d9b80b6ea88b32d662
